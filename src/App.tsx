027b9ae162fbb09fd458c92cc4554abe81dcc220
Abhinay Challa <abhinayc365@gmail.com>
2024-01-01 20:17:15 +0530

    Add main, login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="*" element={<h1>Page not Found</h1>}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
