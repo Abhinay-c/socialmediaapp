bc2c992a9f729bade72e1d90782ba2a83fe52911
Abhinay Challa <abhinayc365@gmail.com>
2024-01-03 10:09:03 +0530

    Update post css

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,22 +1,23 @@
 import { Post as IPost } from "./main";
+import { auth } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface Props {
     post: IPost;
 }
 
 export const Post = (props: Props) => {
+    const [user] = useAuthState(auth);
     const { post } = props;
     return (
         <div className="Post">
-            <div className="title">
-                <h1>{post.title}</h1>
-            </div>
+            <div className="title">{post.title}</div>
             <div className="body">
                 <p>{post.description}</p>
             </div>
             <div className="footer">
-                u/<span> {post.username}</span>
+                by <span> {post.username}</span>
             </div>
         </div>
     );
-}
+};
